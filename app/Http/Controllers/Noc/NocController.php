1bb7817c4ae9635cef1869dd1435fc8f76e0b06b
SailendraRawal <abhishekthapa115@gmail.com>
2025-01-16 12:12:14 +0545

    message send

--- app/Http/Controllers/Noc/NocController.php
+++ app/Http/Controllers/Noc/NocController.php
@@ -102,16 +102,14 @@ class NocController extends Controller
             $message = "{$data->token} is your otp token Nepal Pharmacy Council.";
         
             //  Send SMS using the SmsHandler service
-            // $smsHandler = new SmsHandler();
-            // $response = $smsHandler->send($data->phoneNumber, $message);
-            // if (is_object($response) && isset($response->response_code)) {
-            //     if ($response->response_code === 1001) {
-            //         throw new \Exception('Failed to send OTP via SMS.');
-            //     }
-            // }
-            // segio ui format for 
-            // Ref No in letter head auto 45 
-            // QR code 
+            $smsHandler = new SmsHandler();
+            $response = $smsHandler->send($data->phoneNumber, $message);
+            if (is_object($response) && isset($response->response_code)) {
+                if ($response->response_code === 1001) {
+                    throw new \Exception('Failed to send OTP via SMS.');
+                }
+            }
+          
         
             DB::commit();
     
