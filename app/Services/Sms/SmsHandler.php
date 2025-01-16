d1667b344f8ba644cef01065bb10c582a10b02d7
SailendraRawal <abhishekthapa115@gmail.com>
2025-01-16 12:10:30 +0545

    update the token

--- app/Services/Sms/SmsHandler.php
+++ app/Services/Sms/SmsHandler.php
@@ -13,8 +13,8 @@ class SmsHandler
         $url = "https://api.sparrowsms.com/v2/sms/"; // Replace with your SMS API endpoint
 
         $args = http_build_query(array(
-            'token' => 'v2_2utb0aoNsGFrBHjDC2MZ1i9GZke.NqZH',
-            'from' => 'Demo',
+            'token' => 'v2_a5XZw9x0WlAb27BaZBwcmYpmJYo.3Ogo',
+            'from' => 'Nepal Pharmacy Council',
             'to' => $to,
             'text' => $message,
         ));
