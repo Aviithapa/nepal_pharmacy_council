7c0f2f90999ca34e3f14437fcc982662853e3c1c
SailendraRawal <abhishekthapa115@gmail.com>
2025-01-20 12:23:54 +0545

    update

--- resources/views/admin/pages/cms/noc/index.blade.php
+++ resources/views/admin/pages/cms/noc/index.blade.php
@@ -99,6 +99,9 @@
                                             <a target="_blank" href="{{ getImage($data->pdf_link) }}">
                                                 <span class="badge bg-info-subtle text-info" style="font-size: 18px; text-transform: capitalize;">Print</span>
                                             </a>
+                                            <a class="btn-approve" data-id="{{ $data->id }}" data-status="approved" style="cursor: pointer;">
+                                                <span class="badge bg-success-subtle text-success" style="font-size: 18px; text-transform: capitalize;">Approve</span>
+                                            </a>
                                         @else
                                             <a target="_blank" href="{{ route('noc-main.show', ['noc_main' => $data->id]) }}">
                                                 <span class="badge bg-info-subtle text-info" style="font-size: 18px; text-transform: capitalize;">View</span>
